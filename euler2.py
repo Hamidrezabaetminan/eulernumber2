5863a03b588d8df9e3b8833537f5b186f0ff893f
Hamidrezabaetminan <hbaetminan2001@gmail.com>
2017-07-26 15:38:15 +0000

    Create euler2.py

---
+++ euler2.py
@@ -0,0 +1,16 @@
+def iseven(n):
+    if n % 2 == 0 :
+        return True
+    else:
+        return False
+
+first = 1
+seccond = 2
+sum = 0
+while (first < 4000000):
+    if iseven(first):
+        sum = sum + first
+    new = first + seccond
+    first = seccond
+    seccond = new
+print(sum)
